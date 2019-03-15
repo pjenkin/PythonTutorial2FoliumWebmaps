3f059d7c629ca7540527e985425110261f674007
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-15 14:11:55 +0000

    for loop of list of markers to add to map

--- map1.py
+++ map1.py
@@ -4,10 +4,16 @@ map = folium.Map(location=[51,-4], zoom_start=7, tiles='Mapbox Bright')
 # adding markers
 map.add_child(folium.Marker([50.9,-3.9], popup="Hello, I'm a marker", icon=folium.Icon(color='green')))
 
+
+
 # use feature group in adding markers
 fg = folium.FeatureGroup(name="PJ Map")
 fg.add_child(folium.Marker([50.8,-3.8], popup="Hello, I'm a marker in a feature group #1", icon=folium.Icon(color='green')))
 fg.add_child(folium.Marker([50.7,-4.1], popup="Hello, I'm a marker in a feature group #2", icon=folium.Icon(color='green')))
 map.add_child(fg)
 
-map.save('Map1.html')
+for coordinates in [[50,-4],[50,-5]]:
+    folium.Marker(location=coordinates, popup="Hello, I'm a marker in a feature group #1", icon=folium.Icon(color='green')).add_to(fg)
+
+
+map.save('MapForListMarkers.html')
